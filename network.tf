a0cf97c84db2476b66a7e5e30b1352525802c8f0
Rodrigo <rodrigo.laugeni@hotmail.com>
2021-10-21 13:37:39 -0300

    vm-network

--- network.tf
+++ network.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "rodrigo-vtn" {
     faculdade = "impacta"
   }
 
-depends_on = [azurerm_resource_group.rodrigo]
+depends_on = [ azurerm_resource_group.rodrigo ]
 
 }
 
